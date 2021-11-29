0001d03186afc7b2c787cfc981c0118898f789fc
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2021-11-28 23:28:15 -0300

    using test TDD

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,12 +7,21 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>MapTravel</title>
-        <meta name="description" content="A simple project" />
+        <title>Travel Map</title>
+        <meta
+          name="description"
+          content="A simple project to show my favorite sports in the world."
+        />
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092b" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+          crossOrigin=""
+        />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
